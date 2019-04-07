3fab1b37027d2bdb48e189446381ced4ae45f037
Mariya <mmariya.vyatkina@gmail.com>
2019-04-07 16:10:45 +0300

    edited

--- HW1/gathering.py
+++ HW1/gathering.py
@@ -5,6 +5,7 @@ import json
 import datetime
 from ast import literal_eval
 import pandas as pd
+import configparser
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -12,32 +13,37 @@ logger = logging.getLogger(__name__)
 SCRAPPED_FILE = 'scrapped_data.txt'
 TABLE_FORMAT_FILE = 'data.csv'
 
+
 def gather_process():
     logger.info("gather")
 
-    # gathere data and save as txt
-    access_token = 'here should be your token'
+    # gather data and save as txt
     owner_id = '29534144'
-    count=100
-    offset=-1
-    result=[]
+    count = 100
+    offset = -1
+    result = []
+
+    # In access_token.ini we have token for vk
+    config = configparser.ConfigParser()
+    config.read('access_token.ini')
+    access_token = config.get('settings', 'token')
 
     for _ in range(20):
-        url = 'https://api.vk.com/method/wall.get?owner_id=-{}&count={}&offset={}&v=5.52&access_token={}'.format(owner_id,count,offset+1,access_token)
+        url = 'https://api.vk.com/method/wall.get?owner_id=-{}&count={}&offset={}&v=5.52&access_token={}'.format(
+            owner_id, count, offset + 1, access_token)
         respond = requests.get(url)
         result.append(respond.json())
-        offset+=count
-        
+        offset += count
+
     with open(SCRAPPED_FILE, 'w') as outfile:
         json.dump(result, outfile)
 
+
 def convert_data_to_table_format():
     logger.info("transform")
-    
+
     # transform gathered data from txt file to pandas DataFrame and save as csv
     result = literal_eval(open(SCRAPPED_FILE, 'r').read())
-    n = 100
-    l = len(result)
     ids = []
     dates = []
     comments = []
@@ -52,36 +58,36 @@ def convert_data_to_table_format():
             comments.append(item['comments']['count'])
             likes.append(item['likes']['count'])
             reposts.append(item['reposts']['count'])
-            text.append(item['text'].encode('utf-16','surrogatepass').decode('utf-16'))
-         
-            
+            text.append(item['text'].encode('utf-16', 'surrogatepass').decode('utf-16'))
+
     df = pd.DataFrame({
-        "id": ids, 
-        "date": dates, 
-        "comments_count": comments, 
+        "id": ids,
+        "date": dates,
+        "comments_count": comments,
         "likes_count": likes,
         "reposts_count": reposts,
         "text": text,
     })
-
     df.to_csv(TABLE_FORMAT_FILE, sep='\t')
     pass
 
+
 def stats_of_data():
     logger.info("stats")
-    
+
     # Load pandas DataFrame and print to stdout different statistics about the data.
-    df = pd.read_csv(TABLE_FORMAT_FILE, sep = '\t')
+    df = pd.read_csv(TABLE_FORMAT_FILE, sep='\t')
     df.describe()
-    print('\n', '\033[1m' + '\n','Maximum likes (', df['likes_count'].max(), ') had the following news:' + '\033[0m', '\n')
+    print('\n', '\033[1m' + '\n', 'Maximum likes (', df['likes_count'].max(), ') had the following news:' + '\033[0m',
+          '\n')
     print(df['text'].iloc[df['likes_count'].idxmax()], '\n')
-    print('\033[1m' + '\n','Maximum reposts (', df['reposts_count'].max(), ') had the following news:' + '\033[0m', '\n')
+    print('\033[1m' + '\n', 'Maximum reposts (', df['reposts_count'].max(), ') had the following news:' + '\033[0m',
+          '\n')
     print(df['text'].iloc[df['reposts_count'].idxmax()], '\n')
     print('\033[1m' + 'Maximum comments (', df['comments_count'].max(), ') had the following news:' + '\033[0m', '\n')
     print(df['text'].iloc[df['comments_count'].idxmax()], '\n')
-    print('\033[1m' + '\n','Minimum likes (', df['likes_count'].min(), ') had the following news:' + '\033[0m', '\n')
+    print('\033[1m' + '\n', 'Minimum likes (', df['likes_count'].min(), ') had the following news:' + '\033[0m', '\n')
     print(df['text'].iloc[df['likes_count'].idxmin()], '\n')
-
 
 
 if __name__ == '__main__':
@@ -101,3 +107,4 @@ if __name__ == '__main__':
         stats_of_data()
 
     logger.info("work ended")
+
